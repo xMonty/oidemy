56d98b3754f071bc9d187ffc9fa9bdd2e1712fed
Manoj Subberwal <xmonty@gmail.com>
2021-08-30 18:43:22 +0530

    small menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,38 @@ function Header() {
 
 	return (
 		<header className={styles.container}>
-			<div className={styles.navLogo}><a href='#'>Oidemy</a></div>
-			<a onClick={() => setOpen(!open)}>
-				<div className={`${styles.menuBtn}`}>
+  		<div className={styles.menuBtn}>
+        <a onClick={() => setOpen(!open)}>
 					<span className={open ? `${styles.menuBtn__burger} ` + ` ${styles.open}` : `${styles.menuBtn__burger}`}></span>
-				</div>
-			</a>
+				</a>
+      </div>
+      <div className={styles.navLogo}><a href='#'>Oidemy</a></div>
+    
+      <nav className={open ? `${styles.nav} ` + ` ${styles.open}` : `${styles.nav}`}>
+        <ul className={open ? `${styles.menuNav} ` + ` ${styles.open}` : `${styles.menuNav}`}>
+          <li className={open ? `${styles.menuNav__item} ` + ` ${styles.open}` : `${styles.menuNav__item}`}>
+            <a href="index.html" className={styles.menuNav__link}>
+              Home
+            </a>
+          </li>
+          <li className={open ? `${styles.menuNav__item} ` + ` ${styles.open}` : `${styles.menuNav__item}`}>
+            <a href="index.html" className={styles.menuNav__link}>
+              About Me
+            </a>
+          </li>
+          <li className={open ? `${styles.menuNav__item} ` + ` ${styles.open}` : `${styles.menuNav__item}`}>
+            <a href="index.html" className={styles.menuNav__link}>
+              My Projects
+            </a>
+          </li>
+          <li className={open ? `${styles.menuNav__item} ` + ` ${styles.open}` : `${styles.menuNav__item}`}>
+            <a href="index.html" className={styles.menuNav__link}>
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </nav>
+
 		</header>
 	)
 }
