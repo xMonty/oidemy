395f4c4ac2c90f215ff80029fbc9e142227d8c8f
Manoj Subberwal <xmonty@gmail.com>
2021-09-02 18:18:56 +0530

    footer changes

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,7 +4,15 @@ import styles from './Footer.module.scss';
 function Footer() {
   return (
     <footer className={styles.container}>
-      Footer
+      <div className={styles.sections}>
+        <div>Oidemy Business</div>
+        <div>Lorem ipsum dolor sit amet.</div>
+        <div>Lorem ipsum</div>
+      </div>
+      <div className={styles.sections}></div>
+      <div className={styles.sections}></div>
+      <div className={styles.sections}></div>
+      <div className={styles.sections}></div>
     </footer>
   )
 }
